76c9cc131ff779687612295b32b5a14c75bf63a5
takato256 <c1139952@planet.kanazawa-it.ac.jp>
2023-05-30 14:43:12 +0900

    first commit

---
+++ kit.bat
@@ -0,0 +1,8 @@
+@echo off 
+color E 
+echo K   K    IIIIIII    TTTTTTT 
+echo K  K        I          T 
+echo KKK         I          T 
+echo K  K        I          T 
+echo K   K    IIIIIII       T 
+pause 
